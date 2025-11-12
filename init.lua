1649afc22819884fc4b290d81de3605670da3bd8
Owen Musser <owenmusser29@gmail.com>
2025-11-12 10:01:18 -0500

    changed window movement

--- init.lua
+++ init.lua
@@ -5,41 +5,70 @@ hs.hotkey.bind(hyper, "r", function()
   hs.notify.new({title="Hammerspoon", informativeText="Reloaded"}):send()
 end)
 
-local function tile_window(direction)
-  local win = hs.window.focusedWindow()
-  if not win then
-    return
-  end
-
+function moveWindow(win, direction)
   local screen = win:screen()
   local screen_frame = screen:frame()
   local new_frame = {}
   local win_frame = win:frame()
 
+  local is_on_left_half = win_frame.x < screen_frame.x + (screen_frame.w / 2) - 1
+  
   if direction == "left" then
-    new_frame = {x = screen_frame.x, y = screen_frame.y, w = screen_frame.w / 3, h = screen_frame.h}
+    -- Move to left half (x: 0, y: 0, w: 1/2, h: full)
+    new_frame = {x = screen_frame.x, y = screen_frame.y, w = screen_frame.w / 2, h = screen_frame.h}
   elseif direction == "right" then
-    new_frame = {x = screen_frame.x + (screen_frame.w * 2 / 3), y = screen_frame.y, w = screen_frame.w / 3, h = screen_frame.h}
+    -- Move to right half (x: 1/2, y: 0, w: 1/2, h: full)
+    new_frame = {x = screen_frame.x + (screen_frame.w / 2), y = screen_frame.y, w = screen_frame.w / 2, h = screen_frame.h}
   elseif direction == "up" then
-    new_frame = {x = screen_frame.x + (screen_frame.w / 3), y = screen_frame.y, w = screen_frame.w / 3, h = screen_frame.h}
-  elseif direction == "down" then
-    local is_on_left_side = win_frame.x < screen_frame.x + (screen_frame.w / 2)
-    local is_on_right_side = win_frame.x > screen_frame.x + (screen_frame.w / 2) - 1
-    if is_on_left_side then
-      new_frame = {x = screen_frame.x, y = screen_frame.y, w = screen_frame.w / 2, h = screen_frame.h}
-    elseif is_on_right_side then
-      new_frame = {x = screen_frame.x + (screen_frame.w / 2), y = screen_frame.y, w = screen_frame.w / 2, h = screen_frame.h}
+    -- Moves window to 1/4 of screen, top corner of current half
+    if is_on_left_half then
+      -- Top-Left Quarter (x: 0, y: 0, w: 1/2, h: 1/2)
+      new_frame = {x = screen_frame.x, y = screen_frame.y, w = screen_frame.w / 2, h = screen_frame.h / 2}
     else
-      new_frame = {x = screen_frame.x, y = screen_frame.y, w = screen_frame.w / 2, h = screen_frame.h}
+      -- Top-Right Quarter (x: 1/2, y: 0, w: 1/2, h: 1/2)
+      new_frame = {x = screen_frame.x + (screen_frame.w / 2), y = screen_frame.y, w = screen_frame.w / 2, h = screen_frame.h / 2}
+    end
+  elseif direction == "down" then
+    -- Moves window to 1/4 of screen, bottom corner of current half
+    if is_on_left_half then
+      -- Bottom-Left Quarter (x: 0, y: 1/2, w: 1/2, h: 1/2)
+      new_frame = {x = screen_frame.x, y = screen_frame.y + (screen_frame.h / 2), w = screen_frame.w / 2, h = screen_frame.h / 2}
+    else
+      -- Bottom-Right Quarter (x: 1/2, y: 1/2, w: 1/2, h: 1/2)
+      new_frame = {x = screen_frame.x + (screen_frame.w / 2), y = screen_frame.y + (screen_frame.h / 2), w = screen_frame.w / 2, h = screen_frame.h / 2}
     end
   end
-  win:setFrame(new_frame)
+  
+  -- The check below ensures that we only call setFrame if a move direction was recognized.
+  if new_frame.x ~= nil then
+    win:setFrame(new_frame)
+  end
 end
 
-hs.hotkey.bind(hyper, "left", function() tile_window("left") end)
-hs.hotkey.bind(hyper, "right", function() tile_window("right") end)
-hs.hotkey.bind(hyper, "up", function() tile_window("up") end)
-hs.hotkey.bind(hyper, "down", function() tile_window("down") end)
+hs.hotkey.bind(hyper, "left", function()
+  local win = hs.window.focusedWindow()
+  if win then
+    moveWindow(win, "left")
+  end
+end)
+hs.hotkey.bind(hyper, "right", function()
+  local win = hs.window.focusedWindow()
+  if win then
+    moveWindow(win, "right")
+  end
+end)
+hs.hotkey.bind(hyper, "up", function()
+  local win = hs.window.focusedWindow()
+  if win then
+    moveWindow(win, "up")
+  end
+end)
+hs.hotkey.bind(hyper, "down", function()
+  local win = hs.window.focusedWindow()
+  if win then
+    moveWindow(win, "down")
+  end
+end)
 
 hs.hotkey.bind(hyper, "m", function()
     local win = hs.window.focusedWindow()
